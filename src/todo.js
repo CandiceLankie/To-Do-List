c4cd9f1add0995a08c7d7033ef820304e73d7e15
CandiceLankie <refilwemohlalaa@gmail.com>
2023-09-29 03:57:41 +0200

    added javascript

--- src/todo.js
+++ src/todo.js
@@ -1,5 +1,29 @@
 document.querySelector("#push").onclick = function () {
   if (document.querySelector("#newtask input").value.length == 0) {
-    alert("please Enter a Task");
+    alert("Please Enter a Task");
+  } else {
+    document.querySelector("#tasks").innerHTML += `
+    <div class="task">
+    <span id="taskname">
+    ${document.querySelector("#newtask input").value}
+    </span>
+    <button class="delete">
+    <i class="fa-solid fa-trash-can" style="color: #5a2720;"></i>
+    </button>
+    </div>
+    `;
+    var current_tasks = document.querySelectorAll(".delete");
+    for (var i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+    var tasks = document.querySelectorAll(".task");
+    for (var i = 0; i < tasks.length; i++) {
+      tasks[i].onclick = function () {
+        this.classList.toggle("completed");
+      };
+    }
+    document.querySelector("newtask input").value = "";
   }
 };
